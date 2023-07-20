d62c03eebbcf071963e5d78bfc58c0cca99c28c5
hassam-ud-din <hassam.uddin@carbonteq.com>
2023-07-20 18:13:37 +0500

    basic card ui

--- src/components/Cards/UserCard.tsx
+++ src/components/Cards/UserCard.tsx
@@ -1,16 +1,30 @@
 import React from "react"
+import { Space, Card } from "antd"
+import { LinkOutlined } from "@ant-design/icons"
 
 type Props = {
   id: number
+  login: string
   url: string
+  html_url: string
 }
 
-function UserCard({ id, url }: Props) {
+function UserCard({ id, url, html_url, login }: Props) {
   return (
-    <div>
-      <p>{id}</p>
-      <p>{url}</p>
-    </div>
+    <Space direction="vertical">
+      <Card
+        title={login}
+        extra={
+          <a href={html_url}>
+            <LinkOutlined />
+          </a>
+        }
+        style={{ width: 300 }}
+      >
+        <p>{id}</p>
+        <p>{url}</p>
+      </Card>
+    </Space>
   )
 }
 
